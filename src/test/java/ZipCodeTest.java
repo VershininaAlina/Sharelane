12ac9c2fcb766f5de5c256b7b6f0a24317ba280c
Vershinina Alina <Kirillastrov@gmail.com>
2024-11-13 02:19:11 +0700

    Initial commit

---
+++ src/test/java/ZipCodeTest.java
@@ -0,0 +1,11 @@
+import  org.testing.annotations.Test;
+import  org.openqa.selenium.WebDriver;
+import  org.openqa.selenium.chrome.ChromeDriver;
+
+public class ZipCodeTest {
+
+    @Test
+    public void checkZipCode4Digit {
+        WebDriver  browser = new  ChromeDriver();
+    }
+}
